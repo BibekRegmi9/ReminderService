def1d0fbccc9d3e7d9252d4ebf8f315a5493aab4
Bibek Regmi <1016matrixbibek@gmail.com>
2023-11-06 18:47:16 +0545

    message queues completed

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { createChannel } = require('./utils/messageQueue');
+
 
 // const { sendBasicEmail } = require('./services/email-service');
 const TicketController = require('./controller/ticket-controller');
+const EmailService = require('./services/email-service');
 
 const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
+
 
 const setupAndStartServer = async() => {
     const app = express();
@@ -17,9 +21,12 @@ const setupAndStartServer = async() => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
-    const channel = await createChannel();
 
     app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
 
     app.listen(PORT, () => {
         console.log(`Server started on PORT: ${PORT}`);
